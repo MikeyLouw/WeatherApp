8a1f628463bcab6da2fcf3182a87ecd8121a6bb4
Michael Louw <mike@tangentsolutions.co.za>
2018-03-04 18:50:19 +0200

    Added some models and file system service

---
+++ WeatherApp/WeatherApp/WeatherApp/Services/Interfaces/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+namespace WeatherApp.Services.Interfaces
+{
+    public interface IFileService
+    {
+        void SaveFile(byte[] file, string name);
+        Stream ReadFile(string name);
+        void DeleteFile(string name);
+    }
+}
--- WeatherApp/WeatherApp/WeatherApp/ViewModels/YourWeatherViewModel.cs
+++ WeatherApp/WeatherApp/WeatherApp/ViewModels/YourWeatherViewModel.cs
@@ -1,11 +1,31 @@
 ﻿using System;
+using WeatherApp.Services.Interfaces;
+using Prism.Navigation;
+using Prism.Services;
+using System.Threading.Tasks;
 
 namespace WeatherApp.ViewModels
 {
-    public class YourWeatherViewModel
+    public class YourWeatherViewModel : BaseViewModel
     {
-        public YourWeatherViewModel()
+        IAPIService aPIService;
+
+        public YourWeatherViewModel(INavigationService navigationService, IPageDialogService pageDialogService, IAPIService aPIService):
+        base (navigationService, pageDialogService)
         {
+            this.aPIService = aPIService;
+        }
+
+        public async Task LoadWeatherData(){
+            try
+            {
+                var result = await this.aPIService.GetWeather(new Models.Weather.Welcome());
+
+            }
+            catch (Exception ex)
+            {
+                await pageDialogService.DisplayAlertAsync("Attention", ex.Message, "OK");
+            }
         }
     }
 }
